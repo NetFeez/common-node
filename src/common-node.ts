0c7f4e6170969a6a02729c4fc482633d7ddfbb49
NetFeez <netfeez.dev@gmail.com>
2026-05-06 23:03:28 +0000

    feat: update exports in common-node.ts to remove duplicate File export and ensure proper module structure

--- src/common-node.ts
+++ src/common-node.ts
@@ -1,4 +1,6 @@
+export { File } from './File/File.js';
+export { Async } from './Async.js';
 export { Encoding } from './Encoding.js';
 export { Env } from './Env.js';
-export { File } from './File/File.js';
+export { Glob } from './Glob.js';
 export { Path } from './Path.js';
